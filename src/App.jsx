12ed556ec0d38723157d3463760a023f19954f30
Daniel Kaluhi <kaluhilance4@gmail.com>
2026-06-09 10:00:02 +0300

    fix.updated skills section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import Navbar from './assets/Navbar/navbar'
 import Hero from './assets/Hero/hero'
-import Skills from './assets/Skills/skills'
+import Skills from './assets/skills/skills'
 import Stack from './assets/stack/stack'
 import Projects from './assets/projects/projects'
 import Contact from './assets/contact/contact'
